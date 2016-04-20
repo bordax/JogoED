6c1aa7389d224427bedb49634d17e54c12732d0d
Rafael Bordin <rafael.bordin@live.com>
2016-04-20 14:51:30 -0300

    Adicionei um pequeno exemplo no main

--- main.cpp
+++ main.cpp
@@ -3,19 +3,32 @@
 #include "Pilha.h"
 #include "Baralho.h"
 using namespace std;
-
+int menu_inicial();
 int main()
 {
-    Baralho b;
-    Carta topo;
-    b.embaralhar();
-    for(int index = 0; index < 52; index++){
-        topo = b.Tira_Topo();
-        cout << topo.repr() << endl;
+    if(menu_inicial()==1){
+        string opcao = "S";
+        Baralho b;
+        b.embaralhar();
+
+        cout << "O baralho foi embaralhado!" << endl;
+        Carta topo;
+        do{
+            topo = b.Tira_Topo();
+            cout << "Carta: " << topo.repr() << endl;
+            cout << "Deseja retirar outra carta? (S/N)" <<endl;
+            cin >> opcao;
+        }while (opcao=="S");
+        return 1;
+    }else{
+        return 0;
     }
 
-
-    return 0;
-
-
+}
+int menu_inicial(){
+    cout << "--------Bem vindo ao 21 dos Brother!--------" << endl;
+    cout<< "1. Começar Jogo"<< endl << "2. Sair"<<endl;
+    int op;
+    cin >> op;
+    return op;
 }
